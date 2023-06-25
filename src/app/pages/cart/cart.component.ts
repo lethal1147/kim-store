b35cfff6fcae212bd180856479415e16b68c8fe3
Joakim <yukimever@gmail.com>
2023-06-26 02:57:10 +0700

    feat: first commit of this project

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,63 @@
+import { CartService } from './../../services/cart.service';
+import { Component } from '@angular/core';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html' 
+})
+export class CartComponent {
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 3,
+    id: 2
+  }
+]};
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ]
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit():void {
+    this.cartService.cart.subscribe((_cart: Cart)=> {
+      this.cart = _cart;
+      this.dataSource = this.cart.items
+    })
+  }
+
+  getTotal(items: Array<CartItem>):number {
+    return this.cartService.getTotal(items)
+  }
+
+  onClearCart():void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem) {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem):void {
+    this.cartService.addToCart(item)
+  }
+
+  onRemoveQuantity(item: CartItem):void {
+    this.cartService.removeQuantity(item)
+  }
+}
---
+++ src/app/pages/home/component/filters/filters.component.ts
@@ -0,0 +1,31 @@
+import { Subscription } from 'rxjs';
+import { StoreService } from './../../../../services/store.service';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html'
+})
+export class FiltersComponent implements OnInit, OnDestroy {
+  @Output() showCategory = new EventEmitter<string>();
+  categoriesSubscription: Subscription | undefined;
+  categories: string[] | undefined;
+
+  constructor(private storeService:StoreService) {}
+
+  ngOnInit(): void {
+      this.categoriesSubscription = this.storeService.getAllCategories().subscribe((response) => {
+        this.categories = response.map(category => category.toString())
+      });
+  }
+
+  onShowCategory(category: string): void{
+    this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+      if(this.categoriesSubscription) {
+        this.categoriesSubscription.unsubscribe();
+      }
+  }
+}
